cb8fc15595495f89f186378579993431f75b436a
Michael Koller <koller_michael@outlook.de>
2023-11-19 20:48:57 +0100

    update 2x2.py: incorporate updated genalg.py

--- 2x2.py
+++ 2x2.py
@@ -1,20 +1,26 @@
+import os
+import time
 import genalg
 import numpy as np
 import utils as ut
 import pandas as pd
 
 
+filename = f"2x2_{np.random.randint(100_000_000)}.txt"
+while os.path.exists(filename):
+    filename = f"2x2_{np.random.randint(100_000_000)}.txt"
+print("filename:", filename)
+
 #
 # algorithm parameters
 #
-percent_elite = 1
-percent_kill = 20
-percent_mutation = 40
+percent_elite = 5
+percent_mutation = 5
 
 print_percent = 5
-generations = 100
-population_size = 400
-max_nodes_per_id = 7
+generations = 3000
+population_size = 250
+max_nodes_per_id = 4
 
 #
 # data at which each individual is evaluated to determine its fitness
@@ -31,29 +37,57 @@ performance_metrics = {
     "90%": list(),
     "max": list(),
     "n_Mult": list(),
+    "mean(time_per_generation)": list(),
 }
 
+with open(filename, "w") as fl:
+    fl.write("\n")
+
+# generation 0
 population = genalg.initialize(
     population_size=population_size, max_nodes_per_id=max_nodes_per_id
 )
 
-for i in range(generations):
+fitness = genalg.calc_fitness(population, a, b, c)
+population, fitness = genalg.sort_by_fitness(population, fitness)
+performance_metrics["mean(time_per_generation)"].append(0)
+performance_metrics["generation"].append(0)
+performance_metrics = genalg.stats(population, fitness, performance_metrics)
+print(pd.DataFrame(performance_metrics))
+
+tic_tot = time.time()
+time_tot = 0
+appended_to_file = False
+for i in range(1, generations + 1):
+    tic = time.time()
+    population = genalg.next_generation(
+        population, percent_elite=percent_elite, percent_mutation=percent_mutation
+    )
     fitness = genalg.calc_fitness(population, a, b, c)
     population, fitness = genalg.sort_by_fitness(population, fitness)
+    time_tot += time.time() - tic
 
-    idx_1p0 = np.abs(fitness - 1.0) < 1e-7
+    # should there be a good result, save its source code
+    idx_1p0 = np.abs(fitness - 1.0) < 1e-6
     for count, idx in enumerate(idx_1p0):
         if idx is True:
-            print(ut.unparse(population[count]))
+            ut.append_source(filename, ut.tree_to_source(population[count]))
+            appended_to_file = True
 
     if i % int(print_percent / 100 * generations) == 0:
         performance_metrics["generation"].append(i)
         performance_metrics = genalg.stats(population, fitness, performance_metrics)
-        print(pd.DataFrame(performance_metrics))
 
-    population = genalg.kill_and_repopulate(
-        population,
-        percent_kill=percent_kill,
-        percent_elite=percent_elite,
-        percent_mutation=percent_mutation,
-    )
+        mean_t = time_tot / i
+        performance_metrics["mean(time_per_generation)"].append(mean_t)
+
+        print(pd.DataFrame(performance_metrics))
+        print(
+            f"estimated time remaining: {mean_t * (generations - i) / 60:.2f} minutes"
+        )
+
+if appended_to_file:
+    print("appended to file", filename)
+else:
+    os.remove(filename)
+print(f"Total runtime: {(time.time() - tic_tot) / 60:.2f} minutes.")
