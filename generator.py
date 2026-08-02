839e34ac4da40b3e687e1a499b925d39b6f2926e
Michael Koller <koller_michael@outlook.de>
2026-08-02 19:44:11 +0200

    update generator.py: implement mutate c

--- generator.py
+++ generator.py
@@ -18,7 +18,7 @@ def evaluate(func, *args):
 def gen_one_c(m):
     vars_m = [f"m{i}" for i in range(len(m))]
     num_operands = np.random.randint(1, len(m) + 1)
-    operands = np.sort(choose(vars_m, num_operands))
+    operands = choose(vars_m, num_operands)
     c = [operands[0]]
     if num_operands > 1:
         for i in range(1, num_operands):
@@ -125,54 +125,81 @@ def change_var(var, dim):
         return opt1
 
 
-def mutate(m, c):
-    dim = len(c)
-    if False:  # choose([False, True]):
-        idx = choose(np.arange(len(m)))
-        _, expr, kind = m[idx]
-        if kind == 1:
-            if choose([False, True]):  # left
-                expr[0] = change_var(expr[0], dim)
-            else:  # right
-                expr[2] = change_var(expr[2], dim)
-        elif kind == 2:
-            what = choose([1, 2, 3, 4])
-            if what == 1:  # a_left
-                expr[1] = change_var(expr[1], dim)
-            elif what == 2:  # operator
-                expr[2] = change_op(expr[2])
-            elif what == 3:  # a_right
-                expr[3] = change_var(expr[3], dim)
-            elif what == 4:  # b
-                expr[6] = change_var(expr[6], dim)
-        elif kind == 3:
-            what = choose([1, 2, 3, 4])
-            if what == 1:  # a
-                expr[0] = change_var(expr[0], dim)
-            elif what == 2:  # b_left
-                expr[3] = change_var(expr[3], dim)
-            elif what == 3:  # operator
-                expr[4] = change_op(expr[4])
-            elif what == 4:  # b_right
-                expr[5] = change_var(expr[5], dim)
-        elif kind == 4:
-            what = choose([1, 2, 3, 4, 5, 6])
-            if what == 1:  # a_left
-                expr[1] = change_var(expr[1], dim)
-            elif what == 2:  # operator
-                expr[2] = change_op(expr[2])
-            elif what == 3:  # a_right
-                expr[3] = change_var(expr[3], dim)
-            elif what == 4:  # b_left
-                expr[7] = change_var(expr[7], dim)
-            elif what == 5:  # operator
-                expr[8] = change_op(expr[8])
-            elif what == 6:  # b_right
-                expr[9] = change_var(expr[9], dim)
-    else:
-        pass
+def mutate_c(c, num_mult):
+    """
+    In an equation like `m1 + m3 - m4`, either change one of the variable's
+    index or switch between `+` and `-`.
+    """
+    idx = choose(np.arange(len(c)))  # choose one of c0, c1, c2, ...
+    _, eq = c[idx]
+    if len(eq) == 1:  # there is only one variable
+        c[idx][1][0] = change_var(eq[0], num_mult)
+        return c
+    if (len(eq) == 2 * num_mult - 1) or choose([False, True]):  # change operator
+        num_ops = len(eq) // 2
+        op_idx = choose(np.arange(num_ops)) * 2 + 1
+        eq[op_idx] = change_op(eq[op_idx])
+        c[idx][1] = eq
+        return c
+    else:  # change variable
+        num_vars = len(eq) // 2 + 1
+        var_idx = choose(np.arange(num_vars)) * 2
+        allowed_indices = np.ones(num_mult, dtype=bool)
+        for i in range(num_vars):
+            allowed_indices[int(eq[2 * i][1:])] = False
+        m_new = f"m{choose(np.arange(num_mult)[allowed_indices])}"
+        eq[var_idx] = m_new
+        c[idx][1] = eq
+        return c
 
-    return m, c
+
+def mutate_m(m, dim):
+    """
+    In an equation like `a2 * (b1 + b3)`, either change one of the variable's
+    index or switch between `+` and `-`.
+    """
+    idx = choose(np.arange(len(m)))  # choose one of m0, m1, m2, ...
+    _, expr, kind = m[idx]
+    if kind == 1:
+        if choose([False, True]):  # left
+            expr[0] = change_var(expr[0], dim)
+        else:  # right
+            expr[2] = change_var(expr[2], dim)
+    elif kind == 2:
+        what = choose([1, 2, 3, 4])
+        if what == 1:  # a_left
+            expr[1] = change_var(expr[1], dim)
+        elif what == 2:  # operator
+            expr[2] = change_op(expr[2])
+        elif what == 3:  # a_right
+            expr[3] = change_var(expr[3], dim)
+        elif what == 4:  # b
+            expr[6] = change_var(expr[6], dim)
+    elif kind == 3:
+        what = choose([1, 2, 3, 4])
+        if what == 1:  # a
+            expr[0] = change_var(expr[0], dim)
+        elif what == 2:  # b_left
+            expr[3] = change_var(expr[3], dim)
+        elif what == 3:  # operator
+            expr[4] = change_op(expr[4])
+        elif what == 4:  # b_right
+            expr[5] = change_var(expr[5], dim)
+    elif kind == 4:
+        what = choose([1, 2, 3, 4, 5, 6])
+        if what == 1:  # a_left
+            expr[1] = change_var(expr[1], dim)
+        elif what == 2:  # operator
+            expr[2] = change_op(expr[2])
+        elif what == 3:  # a_right
+            expr[3] = change_var(expr[3], dim)
+        elif what == 4:  # b_left
+            expr[7] = change_var(expr[7], dim)
+        elif what == 5:  # operator
+            expr[8] = change_op(expr[8])
+        elif what == 6:  # b_right
+            expr[9] = change_var(expr[9], dim)
+    return m
 
 
 def substitute(m, c):
@@ -268,16 +295,27 @@ def main():
         # for cs in csubs:
         #     print(cs)
 
-        print("-" * 10)
-        m = gen_m(i, dim)
-        c = gen_c(m, dim)
-        func = gen_func(m, c)
-        print(func)
-        print(evaluate(func, 1, 2, 3, 4, 5, 6))
-        m, c = mutate(m, c)
-        func = gen_func(m, c)
-        print(func)
-        print(evaluate(func, 1, 2, 3, 4, 5, 6))
+        # print("-" * 10)
+        # m = gen_m(i, dim)
+        # c = gen_c(m, dim)
+        # func = gen_func(m, c)
+        # print(func)
+        # print(evaluate(func, 1, 2, 3, 4, 5, 6))
+        # m = mutate_m(m, dim)
+        # func = gen_func(m, c)
+        # print(func)
+        # print(evaluate(func, 1, 2, 3, 4, 5, 6))
+
+        # print("-" * 10)
+        # m = gen_m(i, dim)
+        # c = gen_c(m, dim)
+        # func = gen_func(m, c)
+        # print(func)
+        # print(evaluate(func, 1, 2, 3, 4, 5, 6))
+        # c = mutate_c(c, len(m))
+        # func = gen_func(m, c)
+        # print(func)
+        # print(evaluate(func, 1, 2, 3, 4, 5, 6))
 
 
 if __name__ == "__main__":
